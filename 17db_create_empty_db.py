124ae0939951977187cc654e9cf17aadc029d50f
Radovan Snirc <zwieratko1@gmail.com>
2024-01-02 21:31:49 +0100

    Update 17db_create_empty_db.py

--- 17db_create_empty_db.py
+++ 17db_create_empty_db.py
@@ -9,15 +9,17 @@ dbPath = "{}{}".format(dbDir, dbFile)
 
 try:
     os.makedirs(dbDir)
-    print("DB directory created: {}".format(dbDir))
-except FileExistsError:
-    print("DB directory {} already exists".format(dbDir))
-
-try:
-    con = sqlite3.connect(dbPath)
-    print("OK: connected to", dbPath)
-except sqlite3.Error as error:
-    print("Problem to connect to {}: {}".format(dbPath, error))
-else:
-    con.close()
-    print("OK: connection closed")
+    print("OK: DB directory created - {}".format(dbDir))
+except FileExistsError as error:
+    print("NOK: {}".format(error))
+except PermissionError as error:
+    print("NOK: {}".format(error))
+finally:
+    try:
+        con = sqlite3.connect(dbPath)
+        print("OK: connected to {}".format(dbPath))
+    except sqlite3.Error as error:
+        print("NOK: {}".format(error))
+    else:
+        con.close()
+        print("OK: connection closed")
